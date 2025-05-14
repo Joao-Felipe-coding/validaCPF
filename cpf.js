7682beb51e4be0311d2cf23a4a10c20d570e2705
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-05-13 21:47:02 -0300

    validação do segundo diigto do cpf em cpf.js

--- cpf.js
+++ cpf.js
@@ -18,4 +18,24 @@ function validarCPF(cof) {
   if (resto === 10 || resto === 11) {
     resto = 0;
   }
+
+  if (resto !== Nummber.parseInt(cpf.substring(9, 10))) {
+    return false;
+  }
+
+  //validação do 2° DV
+  soma = 0;
+  for (let i = 0; i <= 10; i++) {
+    soma += Number.parseInt(cpf.substring(i - 1, i)) * (12 - i);
+  }
+
+  resto = (soma * 10) % 11;
+  if (resto === 10 || resto === 11) {
+    resto = 0;
+  }
+  if (resto !== Number.parseInt(cpf.substring(10, 10))) {
+    return false;
+  }
+
+  return true;git 
 }
